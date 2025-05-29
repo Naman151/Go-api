cb66e26a52c160e02b787cea82e64cf1d05e3398
Naman151 <namanshahi151@gmail.com>
2025-05-29 13:27:47 +0530

    server working

--- cmd/students-api/main.go
+++ cmd/students-api/main.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/Naman151/Go-api/internal/config"
+)
 
 func main()  {
-	fmt.Println("Welcome")
+	// Load Config
+	cfg := config.MustLoad()
+
+	//setup router
+	router := http.NewServeMux()
+
+	router.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Printf("Server Working")
+		w.Write([]byte("Welcome to Students Api"))
+	})
+
+	//setup server
+	server := http.Server{
+		Addr: cfg.Addr,
+		Handler: router,
+	}
+
+	err := server.ListenAndServe()
+
+	if err != nil {
+		log.Fatalf("Failed to Start Server %s", err.Error())
+	}
+	defer server.Close()
 }
